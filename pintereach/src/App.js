bb925ae9b3c9f762d3dff14c675559f1fe54634f
brittanymclaughlin <britt@brittanytroutman.com>
2020-07-27 22:14:32 -0400

    My changes as of right now

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import styled from 'styled-components';
+import AddArticleForm from './components/AddArticleForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <NavBar />
+      
+    </Container>
   );
 }
+const Container = styled.div`
+    margin: 0px;
+`
 
 export default App;
