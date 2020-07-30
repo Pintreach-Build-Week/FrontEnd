a5ff6ef0615d498349a59da1863ee5035e38e059
Evelyn Nguyen <Evelyn@Viets-iMacx.local>
2020-07-30 15:44:22 -0700

    edit/delete

--- pintereach/src/utils/AxiosWithAuth.js
+++ pintereach/src/utils/AxiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const AxiosWithAuth = () => {
     const token = localStorage.getItem("token");
     return axios.create({
-        baseURL: "https://bw-pt-pintereach-1-ebi-backend.herokuapp.com/api/auth",  //input in api
+        baseURL: "https://bw-pt-pintereach-1-ebi-backend.herokuapp.com",  //input in api
         headers: {
             Authorization: token
         }
